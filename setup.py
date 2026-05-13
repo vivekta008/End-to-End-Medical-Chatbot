6d867c63e0151eea86ff02709f3443ff84f937fe
vivekta008 <tavivek256@gmail.com>
2026-05-13 21:49:46 +0530

    packed the package under setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1',
+    author='vivekta',
+    author_email="tavivek256@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)   
